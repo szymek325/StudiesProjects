e1ff34ce5b51f4a1805c2512c8c710d9e0bb8307
Mateusz Szymkowiak <mateusz.ma.szymkowiak@student.put.poznan.pl>
2017-11-06 14:14:36 +0100

    Basic Regex

---
+++ SnmpAgent/Mib.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace SnmpAgent
+{
+    public class Mib
+    {
+        public string Path { get; set; } 
+        public string Text { get; set; }
+
+        public void ReadFile()
+        {
+            var streamReader = new StreamReader(Constants.Path);
+            this.Text = streamReader.ReadToEnd();
+        }
+    }
+}
---
+++ SnmpAgent/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace SnmpAgent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myPattern = @"(\w*\s* )OBJECT-TYPE\s* SYNTAX(.*? )ACCESS(.*? )STATUS(.*? )DESCRIPTION\s * ""(.*?)""\s *::=\s *{.*?}";
+            var myRegex= new Regex(myPattern,RegexOptions.Singleline);
+
+
+
+            var mib= new Mib();
+            mib.ReadFile();
+            //Console.WriteLine(mib.Path);
+            //Console.WriteLine(mib.Text);
+            Console.WriteLine(myPattern);
+
+            Console.WriteLine("\n" + "*** First Match ***");
+            var oneMatch = myRegex.Match(mib.Text);
+            if (oneMatch.Success)
+            {
+                Console.WriteLine("Overall Match: " + oneMatch.Groups[0].Value);
+                Console.WriteLine("Group 1: " + oneMatch.Groups[1].Value);
+                Console.WriteLine("Group 2: " + oneMatch.Groups[2].Value);
+                Console.WriteLine("Group 3: " + oneMatch.Groups[3].Value);
+                Console.WriteLine("Group 4: " + oneMatch.Groups[4].Value);
+                Console.WriteLine("Group 5: " + oneMatch.Groups[5].Value);
+            }
+
+
+            //Console.WriteLine("\n" + "*** Matches ***");
+            //var allMatches = myRegex.Matches(mib.Text);
+            //if (allMatches.Count > 0)
+            //{
+            //    foreach (Match SomeMatch in allMatches)
+            //    {
+            //        Console.WriteLine("Overall Match: " + SomeMatch.Groups[0].Value);
+            //        Console.WriteLine("Group 1: " + SomeMatch.Groups[1].Value);
+            //        Console.WriteLine("Group 2: " + SomeMatch.Groups[2].Value);
+            //        Console.WriteLine("Group 3: " + SomeMatch.Groups[3].Value);
+            //    }
+            //}
+
+
+            Console.ReadKey();
+        }
+    }
+}
