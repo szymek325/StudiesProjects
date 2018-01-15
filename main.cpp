8a095770d5f3aaecacd6340b38cf1b5386ded532
mateusz <matszym325@gmail.com>
2018-01-15 23:23:30 +0100

    succesful crop and putText

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ Mat imgGray;
 int thresh = 52;
 int max_thresh = 255;
 RNG rng(12345);
+Scalar boundingColor = Scalar((0, 0), (0, 0), (0, 0));
 
 void detectContours(int, void* );
 void detectCircles(int,void*);
@@ -39,7 +40,6 @@ int main() {
 
 void detectContours(int, void* ) //detect and draw contours
 {
-
     Mat contoursImg;
     Mat canny_output;
     vector<vector<Point> > contours;
@@ -48,32 +48,69 @@ void detectContours(int, void* ) //detect and draw contours
     cvtColor( img, contoursImg, COLOR_BGR2GRAY );
     blur( contoursImg, contoursImg, Size(3,3) );
     Canny( contoursImg, canny_output, thresh, thresh*2, 3 );
-    findContours( canny_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0) );
+    findContours( canny_output, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE, Point(0, 0) );
 
     Mat drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
+    Mat mask = Mat::zeros(canny_output.rows, canny_output.cols, CV_8UC1);
 
     vector<vector<Point> > contours_poly(contours.size());
+    vector<float> radius(contours.size());
+    vector<Point2f> center(contours.size());
 
     for (size_t i = 0; i < contours.size(); i++) {
         approxPolyDP(contours[i], contours_poly[i], arcLength(Mat(contours[i]), true)*0.02, true);
+        minEnclosingCircle(contours_poly[i], center[i], radius[i]);
     }
 
     for( size_t i = 0; i< contours_poly.size(); i++ )
     {
+        if(contours_poly[i].size()==8){
+            Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+            drawContours( drawing, contours_poly, (int)i, color, 2, 8, hierarchy, 0, Point() );
+            putText(img, "CIRCLE", center[i], FONT_HERSHEY_COMPLEX_SMALL, 0.8, boundingColor, 1,CV_AA);
+
+            drawContours(mask, contours,i, Scalar(255), CV_FILLED);
+        }
         if(contours_poly[i].size()==5){
             Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
             drawContours( drawing, contours_poly, (int)i, color, 2, 8, hierarchy, 0, Point() );
+            putText(img, "PENTAGON", center[i], FONT_HERSHEY_COMPLEX_SMALL, 0.8, boundingColor, 1,CV_AA);
+
+            drawContours(mask, contours, i, Scalar(255), CV_FILLED);
         }
         else if(contours_poly[i].size()==4){
             Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
             drawContours( drawing, contours_poly, (int)i, color, 2, 8, hierarchy, 0, Point() );
+            putText(img, "RECTANGLE", center[i], FONT_HERSHEY_COMPLEX_SMALL, 0.8, boundingColor, 1,CV_AA);
+
+            drawContours(mask, contours, i, Scalar(255), CV_FILLED);
         }
         else if(contours_poly[i].size()==3) {
             Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
             drawContours(drawing, contours_poly, (int) i, color, 2, 8, hierarchy, 0, Point());
+            putText(img, "TRIANGLE", center[i], FONT_HERSHEY_COMPLEX_SMALL, 0.8, boundingColor, 1,CV_AA);
+
+            drawContours(mask, contours, i, Scalar(255), CV_FILLED);
         }
 
     }
+
+    Mat crop(img.rows, img.cols, CV_8UC3);
+
+    // set background to green
+    crop.setTo(Scalar(255,255,255));
+
+    // and copy the magic apple
+    img.copyTo(crop, mask);
+
+    // normalize so imwrite(...)/imshow(...) shows the mask correctly!
+    normalize(mask.clone(), mask, 0.0, 255.0, CV_MINMAX, CV_8UC1);
+
+    namedWindow( "MASK", WINDOW_AUTOSIZE );
+    imshow( "MASK", mask );
+
+    namedWindow( "CROP", WINDOW_AUTOSIZE );
+    imshow( "CROP", crop );
 
     namedWindow( "Contours", WINDOW_AUTOSIZE );
     imshow( "Contours", drawing );
